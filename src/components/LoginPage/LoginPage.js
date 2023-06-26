edaab96120e1ffd05099db46de0736b553224283
TaftSound <mattmurphytattoos@gmail.com>
2023-06-26 15:23:35 -0700

    Hide extra buttons for small views on login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -11,6 +11,16 @@ import { FlexBox } from "../styled-components"
 import { FormButton } from "../StyledButtons/FormButton"
 import { UserLoginPopup } from "../UserLoginPopup/UserLoginPopup"
 
+const PhoneViewHide = styled.div`
+  visibility: hidden;
+  display: none;
+
+@media (min-width: 600px) {
+    visibility: visible;
+    display: block;
+  }
+`
+
 const H1 = styled.h1`
   color: ${MAIN_FONT_COLOR};
   font-size: 26px;
@@ -81,9 +91,9 @@ const LoginPage = (props) => {
                       to demonstrate proficiency in modern web development.</H2>
                       {/* <H2>Please create a new account or login as guest to experience the various features of Tweeter:</H2> */}
                       <FlexBox  gap="15px">
-                        <FormButton google={true} onClick={userSignup}><FlexBox margin="0px 25px">Google sign up</FlexBox></FormButton>
+                        <PhoneViewHide><FormButton google={true} onClick={userSignup}><FlexBox margin="0px 25px">Google sign up</FlexBox></FormButton></PhoneViewHide>
                         <FormButton colorButton={true} onClick={guestLogin}>Guest login</FormButton>
-                        <FormButton dark="true" onClick={explainAccounts}>What's the difference?</FormButton>
+                        <PhoneViewHide><FormButton dark="true" onClick={explainAccounts}>What's the difference?</FormButton></PhoneViewHide>
                       </FlexBox>
                       <H2>Once logged in you may enjoy a variety of exciting features:</H2>
                       {/* <H2>Once you have signed in, please enjoy the various features availabe on Tweeter:</H2> */}
